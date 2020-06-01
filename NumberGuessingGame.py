acac9a1b2b61ed937501a284813037722d50081d
Aditya Lakshminarayanan <aditya.net09@gmail.com>
2020-06-01 17:05:11 -0600

    Cleaned up the code

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,42 +1,46 @@
 #This is a twist on the classic number game
+#Here a computer itself tries to guess the number guessed by the computer
+#using trial and error
 
 import random
 
 print("Computer: Hello, My name is Computer. What is your name?\n")
 
 #Computer Name
+#Creating a random name using the alphabets 
+#Adding the alphabets to the empty lists
+#and assigning the name using random() functions
 computerName = ""
 alphabetList = []
+
 for i in range(97, 123):
   alphabetList.append(chr(i))
 
-
-for key in range(random.randint(2, 10)):
+for key in range(random.randint(1, 10)):
     i = random.randint(1, 26)
     nameCharacter = alphabetList[i]
     computerName = computerName + nameCharacter
 
-print(f"{computerName}: Hello, Computer. My name is {computerName}")
+print(f"{computerName}: Hello, Computer. My name is {computerName}\n")
 
 print(f"Well, {computerName}, I am thinking of a number between 1 and 20")
-print("You will have 6 guesses.")
+print("You will have 6 guesses.\n")
 
 #Selecting a random number between 1 and 20
 secretNumber = random.randint(1, 20)
-#print(f"DEBUG:The secret number is : {secretNumber}")
 
 #Computer Guessing:
 computerList = [i for i in range(1, 21)]
 low = computerList[0]
 high = computerList[19]
- #mid = (low + high)// 2
-guessesTaken = 1
 
+guessesTaken = 1
 
 while guessesTaken != 6:
   mid = (low + high)// 2
   print("Take a guess.")
   computerGuess = mid
+
   if computerGuess > secretNumber:
     print(mid)
     print("Your guess is too high")
